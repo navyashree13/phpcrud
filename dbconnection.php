bc4023f0a96d7b7c1e51b9dab9fca853cd5da833
navyashree13 <navyashree.k@moolya.com>
2021-05-08 18:23:26 +0530

    heroku db changes

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,9 @@
 <?php
-$con=mysqli_connect("localhost", "root", "root", "test","3306");
+//local
+//$con=mysqli_connect("localhost", "root", "root", "test","3306");
+
+//heroku
+$con=mysqli_connect("sql6.freemysqlhosting.net", "sql6410958", "HS8uC6DQ2U", "sql6410958","3306");
 if(mysqli_connect_errno())
 {
 echo "Connection Fail".mysqli_connect_error();
